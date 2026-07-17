aeb3db128f3cfa984422bb98d192e0ed9ed1efa4
Puspaaamm <pushpam010203@gmail.com>
2026-07-17 07:51:46 +0530

    Create user-config.py

---
+++ user-config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+family = 'wikipedia'
+mylang = 'hi'
+
+# Replace 'LinkRepairerBot' with your actual registered Wikipedia bot username
+usernames['wikipedia']['hi'] = 'LinkRepairerBot'
+
+# Points Pywikibot to your password configuration file
+password_file = "user-password.py"
+
